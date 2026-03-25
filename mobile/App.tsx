79d80d0b72a03ea2510fb3eb033dedb0c8aa5f5f
BartoszOlszak47 <bartosz.olszak47@o2.pl>
2026-03-25 19:49:42 +0100

    narazie ekrany bez podpięcia

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,55 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
+import MainKierowca from './screens/MainKierowca'
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  // Funkcja, która odpali się po kliknięciu przycisku
-  const handlePress = () => {
-    Alert.alert("Brum brum! 🚕", "Twoja wirtualna taksówka jest w drodze!");
-  };
-
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>System TAXI</Text>
-      <Text style={styles.subtitle}>Test połączenia z telefonem</Text>
-
-      {/* Nasz główny przycisk */}
-      <TouchableOpacity style={styles.button} onPress={handlePress}>
-        <Text style={styles.buttonText}>Zamów wirtualne auto</Text>
-      </TouchableOpacity>
-
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="MainKierowca" component={MainKierowca} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-// Tutaj definiujemy wygląd (CSS dla aplikacji mobilnych)
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#1E1E1E', // Ciemny motyw
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 36,
-    fontWeight: 'bold',
-    color: '#FFD700', // Taksówkowy żółty
-    marginBottom: 10,
-  },
-  subtitle: {
-    fontSize: 16,
-    color: '#AAAAAA',
-    marginBottom: 40,
-  },
-  button: {
-    backgroundColor: '#FFD700', 
-    paddingVertical: 15,
-    paddingHorizontal: 30,
-    borderRadius: 25, // Zaokrąglone rogi
-  },
-  buttonText: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: '#000000',
-  },
-});
