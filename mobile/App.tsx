8f79fdf37b4ba1e934c94db66356b460da95ca5a
BartoszOlszak47 <bartosz.olszak47@o2.pl>
2026-03-28 19:15:23 +0100

    reszta ekranów
    narazie jak się wpisze w loginie kierowca/pracownik to przekieruje do innych ekranów, narazie wszystko na przykładowych danych

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
-import MainKierowca from './screens/MainKierowca'
+import MainKierowca from './screens/MainKierowca';
+import MenuKierowca from './screens/MenuKierowca';
+import HistoriaKierowca from './screens/HistoriaKierowca';
+import MenuPracownik from './screens/MenuPracownik';
+import HistoriaPracownik from './screens/HistoriaPracownik';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,7 +15,13 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={LoginScreen} />
+        
         <Stack.Screen name="MainKierowca" component={MainKierowca} />
+        <Stack.Screen name="MenuKierowca" component={MenuKierowca} />
+        <Stack.Screen name="HistoriaKierowca" component={HistoriaKierowca} />
+
+        <Stack.Screen name="MenuPracownik" component={MenuPracownik} />
+        <Stack.Screen name="HistoriaPracownik" component={HistoriaPracownik} />
       </Stack.Navigator>
     </NavigationContainer>
   );
