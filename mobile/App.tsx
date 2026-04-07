e972627b71f7ccf96c9aafbc493923dbf59f2705
BartoszOlszak47 <bartosz.olszak47@o2.pl>
2026-04-07 12:23:06 +0200

    poprawki

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import LoginScreen from './screens/LoginScreen';
 import MainKierowca from './screens/MainKierowca';
-import MenuKierowca from './screens/MenuKierowca';
+import ZleceniaKierowca from './screens/ZleceniaKierowca';
 import HistoriaKierowca from './screens/HistoriaKierowca';
-import MenuPracownik from './screens/MenuPracownik';
+import ZamowieniePracownik from './screens/ZamowieniePracownik';
 import HistoriaPracownik from './screens/HistoriaPracownik';
 
 const Stack = createNativeStackNavigator();
@@ -13,14 +14,19 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
         <Stack.Screen name="Login" component={LoginScreen} />
         
         <Stack.Screen name="MainKierowca" component={MainKierowca} />
-        <Stack.Screen name="MenuKierowca" component={MenuKierowca} />
+        <Stack.Screen name="ZleceniaKierowca" component={ZleceniaKierowca} />
         <Stack.Screen name="HistoriaKierowca" component={HistoriaKierowca} />
-
-        <Stack.Screen name="MenuPracownik" component={MenuPracownik} />
+        
+        <Stack.Screen name="ZamowieniePracownik" component={ZamowieniePracownik} />
         <Stack.Screen name="HistoriaPracownik" component={HistoriaPracownik} />
       </Stack.Navigator>
     </NavigationContainer>
